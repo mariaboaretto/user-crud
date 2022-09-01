a7a8a969d9c3e77825f4e06ebfd9a8e7d3c30592
Maria Boaretto <mariaboarettocampos@gmail.com>
2022-09-01 10:51:07 -0400

    Fixing Link for Footer nav

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
     return (
         <footer className="fixed-bottom bg-light mt-3 pt-3">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                 <li className="nav-item">
-                    <a href="/" className="nav-link px-2 text-dark">Home</a>
+                    <Link to="/" className="nav-link px-2 text-dark">Home</Link>
                 </li>
                 <li className="nav-item">
-                    <a href="/about" className="nav-link px-2 text-dark">About</a>
+                    <Link to="/about" className="nav-link px-2 text-dark">About</Link>
                 </li>
             </ul>
             <p className="text-center pb-3 mb-3 text-muted">
